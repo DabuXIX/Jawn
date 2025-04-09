7d32caba6f4943d27dd8e66a21bbe6a265217ae2
DabuXIX <139291246+DabuXIX@users.noreply.github.com>
2025-04-09 14:34:04 -0400

    Update Debug.c

--- Debug.c
+++ Debug.c
@@ -1,4 +1,3 @@
-CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
-ITM->LAR  = 0xC5ACCE55;      // Unlock ITM (optional for STM32C0)
-ITM->TCR  = ITM_TCR_ITMENA_Msk | ITM_TCR_TSENA_Msk | ITM_TCR_SYNCENA_Msk;
-ITM->TER  = 1;               // Enable stimulus port 0
+CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk; // Enable trace
+ITM->TCR = ITM_TCR_ITMENA_Msk;                  // Enable ITM
+ITM->TER = 1;                                   // Enable stimulus port 0
